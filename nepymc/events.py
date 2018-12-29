ff382511ac715e4d6829e1947475abd8fd21d8b0
Dave Andreoli <dave@gurumeditation.it>
2018-12-29 19:56:51 +0100

    Events and inputEvents in

---
+++ nepymc/events.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+#
+# Copyright (C) 2010-2019 Davide Andreoli <dave@gurumeditation.it>
+#
+# This file is part of NEPYMC, a free Media Center written in Python.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Standard events:
+
+PLAYBACK_STARTED
+PLAYBACK_FINISHED
+PLAYBACK_PAUSED
+PLAYBACK_UNPAUSED
+PLAYBACK_SEEKED
+
+VOLUME_CHANGED
+STORAGE_CHANGED
+KEEP_ALIVE
+
+BROWSER_PAGE_CHANGED ??
+BROWSER_VIEW_CHANGED ??
+MAINMENU_SHOWED ??
+MAINMENU_HIDDEN ??
+
+"""
+
+
+def DBG(msg):
+    # print('EVENTS: %s' % msg)
+    pass
+
+
+_listeners = []
+_listeners_single = []
+
+
+def listener_add(name, func, *a, **ka):
+    """ Add a new listener to the events chain.
+        The callback will be called until you delete the listener.
+    """
+    _listeners.append((name, func, a, ka))
+
+    DBG('Listener Add: ' + name)
+    for (name, cb, a, ka) in _listeners:
+        DBG('  * ' + name)
+
+
+def listener_add_single_shot(event, func, *a, **ka):
+    """ Add a new listener to the specific event.
+        The callback will be called only one time.
+    """
+    _listeners_single.append((event, func, a, ka))
+
+
+def listener_del(name):
+    """ Remove a listener by name """
+
+    DBG('Listener Del: ' + name)
+
+    for lis in _listeners:
+        (n, cb, a, ka) = lis
+        if n == name:
+            _listeners.remove(lis)
+            return
+
+
+def event_emit(event):
+    """ Emit the given event to all listeners, event is just a string """
+
+    DBG('Emit Event: ' + event + '  listeners: ' + str(len(_listeners)))
+
+    for lis in _listeners_single:
+        (evt, cb, a, ka) = lis
+        if evt == event:
+            cb(*a, **ka)
+            _listeners_single.remove(lis)
+
+    for lis in _listeners:
+        (name, cb, a, ka) = lis
+        cb(event, *a, **ka)
---
+++ nepymc/input_events.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+#
+# Copyright (C) 2010-2019 Davide Andreoli <dave@gurumeditation.it>
+#
+# This file is part of NEPYMC, a free Media Center written in Python.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from nepymc import events
+
+
+def DBG(msg):
+    # print('INPUT_EVENTS: %s' % msg)
+    pass
+
+
+STANDARD_EVENTS = """
+UP DOWN LEFT RIGHT OK BACK EXIT
+PLAY STOP PAUSE TOGGLE_PAUSE
+FORWARD BACKWARD FAST_FORWARD FAST_BACKWARD
+PLAYLIST_NEXT PLAYLIST_PREV PLAYLIST_CHANGED
+VOLUME_UP VOLUME_DOWN VOLUME_MUTE
+SUBS_DELAY_MORE SUBS_DELAY_LESS SUBS_DELAY_ZERO
+TOGGLE_FULLSCREEN
+VIEW_LIST VIEW_POSTERGRID VIEW_COVERGRID
+BIGGER SMALLER
+TOGGLE_DVD_MENU
+"""
+
+EVENT_CONTINUE = True
+EVENT_BLOCK = False
+
+_listeners = []
+
+
+def listener_add(name, event_cb, cb_data=None):
+    global _listeners
+
+    _listeners.append((name, event_cb, cb_data))
+
+    DBG('Listener Add: ' + name)
+    for lis in _listeners:
+        (name, cb, data) = lis
+        DBG('  * ' + name)
+
+
+def listener_del(name):
+    global _listeners
+
+    DBG('Listener Del: ' + name)
+    for lis in _listeners:
+        (n, cb, data) = lis
+        if n == name:
+            _listeners.remove(lis)
+            return
+
+
+def listener_promote(name):
+    global _listeners
+
+    DBG('Listener Promote: ' + name)
+    for lis in _listeners:
+        (n, cb, data) = lis
+        if n == name:
+            _listeners.remove(lis)
+            _listeners.append(lis)
+            return
+
+
+def event_emit(event):
+    DBG('Emit Event: ' + event + '  listeners: ' + str(len(_listeners)))
+
+    events.event_emit('KEEP_ALIVE')
+
+    for lis in reversed(_listeners):
+        (name, cb, data) = lis
+
+        if data:
+            res = cb(event, data)
+        else:
+            res = cb(event)
+
+        # print("  ->  '%s' (%s)" %  (name, ('continue' if res else 'block')))
+
+        if res == EVENT_BLOCK:
+            return
